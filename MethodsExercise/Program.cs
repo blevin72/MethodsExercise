4b1b820a1a7942531e8f14ee8d26e1bb5a2ee7f1
Bryan Levin <bryanlevin72@gmail.com>
2023-06-19 08:59:11 -0700

    Conversation w/Code

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hi there, What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Hi {name}, nice to meet you. What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"That's crazy! {color} is my favorite color too. What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"I see! I am not a big fan of {animal}, I am more of a horse person myself. Do you like music? What is your favorite band?");
+            var band = Console.ReadLine();
         }
     }
 }
